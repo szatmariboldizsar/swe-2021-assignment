138936fc795fcc0f382b87a9d6396e69c595abe2
Szatmári Boldizsár <szatmariboldizsar@gmail.com>
2021-05-10 22:19:23 +0200

    Main.java initialized

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+
+public class Main {
+    /**
+     * Launches the application.
+     */
+    public static void main(String[] args) {
+        Application.launch(BoardGameApplication.class ,args);
+    }
+}
