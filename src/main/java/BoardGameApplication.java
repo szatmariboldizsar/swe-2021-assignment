365cccc4afe7ad799577c4df59edda389cb2bc2c
Szatmári Boldizsár <szatmariboldizsar@gmail.com>
2021-05-10 22:19:58 +0200

    BoardGameApplication.java initialized

---
+++ src/main/java/BoardGameApplication.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BoardGameApplication extends Application {
+
+    /**
+     * Sets up the application window.
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/ui.fxml"));
+        stage.setTitle("2.4 Board Game");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
