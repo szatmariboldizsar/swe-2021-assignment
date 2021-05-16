48fe6242f52686b0670842c196e48005576a55a4
Szatmári Boldizsár <szatmariboldizsar@gmail.com>
2021-05-17 00:56:21 +0200

    Application base added

---
+++ src/main/java/boardgame/BoardGameController.java
@@ -0,0 +1,183 @@
+package boardgame;
+
+import boardgame.model.BoardGameModel;
+import boardgame.model.RedDirection;
+import boardgame.model.Position;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import org.tinylog.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardGameController {
+
+    private enum SelectionPhase {
+        SELECT_FROM,
+        SELECT_TO;
+
+        public SelectionPhase alter() {
+            return switch (this) {
+                case SELECT_FROM -> SELECT_TO;
+                case SELECT_TO -> SELECT_FROM;
+            };
+        }
+    }
+
+    private SelectionPhase selectionPhase = SelectionPhase.SELECT_FROM;
+
+    private List<Position> selectablePositions = new ArrayList<>();
+
+    private Position selected;
+
+    private BoardGameModel model = new BoardGameModel();
+
+    @FXML
+    private GridPane board;
+
+    @FXML
+    private void initialize() {
+        createBoard();
+        createPieces();
+        setSelectablePositions();
+        showSelectablePositions();
+    }
+
+    private void createBoard() {
+        for (int i = 0; i < board.getRowCount(); i++) {
+            for (int j = 0; j < board.getColumnCount(); j++) {
+                var square = createSquare(i, j);
+                board.add(square, j, i);
+            }
+        }
+    }
+
+    private StackPane createSquare(int i, int j) {
+        var square = new StackPane();
+        square.getStyleClass().add("square");
+        square.setOnMouseClicked(this::handleMouseClick);
+        return square;
+    }
+
+    private void createPieces() {
+        for (int i = 0; i < model.getPieceCount(); i++) {
+            model.positionProperty(i).addListener(this::piecePositionChange);
+            var piece = createPiece(Color.valueOf(model.getPieceType(i).name()));
+            getSquare(model.getPiecePosition(i)).getChildren().add(piece);
+        }
+    }
+
+    private Circle createPiece(Color color) {
+        var piece = new Circle(50);
+        piece.setFill(color);
+        return piece;
+    }
+
+    @FXML
+    private void handleMouseClick(MouseEvent event) {
+        var square = (StackPane) event.getSource();
+        var row = GridPane.getRowIndex(square);
+        var col = GridPane.getColumnIndex(square);
+        var position = new Position(row, col);
+        Logger.info("Click on square {}", position);
+        handleClickOnSquare(position);
+    }
+
+    private void handleClickOnSquare(Position position) {
+        switch (selectionPhase) {
+            case SELECT_FROM -> {
+                if (selectablePositions.contains(position)) {
+                    selectPosition(position);
+                    alterSelectionPhase();
+                }
+            }
+            case SELECT_TO -> {
+                if (selectablePositions.contains(position)) {
+                    var pieceNumber = model.getPieceNumber(selected).getAsInt();
+                    var direction = RedDirection.of(position.row() - selected.row(), position.col() - selected.col());
+                    Logger.info("Moving piece {} {}", pieceNumber, direction);
+                    model.move(pieceNumber, direction);
+                    deselectSelectedPosition();
+                    alterSelectionPhase();
+                }
+            }
+        }
+    }
+
+    private void alterSelectionPhase() {
+        selectionPhase = selectionPhase.alter();
+        hideSelectablePositions();
+        setSelectablePositions();
+        showSelectablePositions();
+    }
+
+    private void selectPosition(Position position) {
+        selected = position;
+        showSelectedPosition();
+    }
+
+    private void showSelectedPosition() {
+        var square = getSquare(selected);
+        square.getStyleClass().add("selected");
+    }
+
+    private void deselectSelectedPosition() {
+        hideSelectedPosition();
+        selected = null;
+    }
+
+    private void hideSelectedPosition() {
+        var square = getSquare(selected);
+        square.getStyleClass().remove("selected");
+    }
+
+    private void setSelectablePositions() {
+        selectablePositions.clear();
+        switch (selectionPhase) {
+            case SELECT_FROM -> selectablePositions.addAll(model.getPiecePositions());
+            case SELECT_TO -> {
+                var pieceNumber = model.getPieceNumber(selected).getAsInt();
+                for (var direction : model.getValidMoves(pieceNumber)) {
+                    selectablePositions.add(selected.moveTo(direction));
+                }
+            }
+        }
+    }
+
+    private void showSelectablePositions() {
+        for (var selectablePosition : selectablePositions) {
+            var square = getSquare(selectablePosition);
+            square.getStyleClass().add("selectable");
+        }
+    }
+
+    private void hideSelectablePositions() {
+        for (var selectablePosition : selectablePositions) {
+            var square = getSquare(selectablePosition);
+            square.getStyleClass().remove("selectable");
+        }
+    }
+
+    private StackPane getSquare(Position position) {
+        for (var child : board.getChildren()) {
+            if (GridPane.getRowIndex(child) == position.row() && GridPane.getColumnIndex(child) == position.col()) {
+                return (StackPane) child;
+            }
+        }
+        throw new AssertionError();
+    }
+
+    private void piecePositionChange(ObservableValue<? extends Position> observable, Position oldPosition, Position newPosition) {
+        Logger.info("Move: {} -> {}", oldPosition, newPosition);
+        StackPane oldSquare = getSquare(oldPosition);
+        StackPane newSquare = getSquare(newPosition);
+        newSquare.getChildren().addAll(oldSquare.getChildren());
+        oldSquare.getChildren().clear();
+    }
+
+}
---
+++ src/main/java/boardgame/model/RedDirection.java
@@ -0,0 +1,43 @@
+package boardgame.model;
+
+public enum RedDirection implements Direction {
+
+    UP_LEFT(-1, -1),
+    UP(-1, 0),
+    UP_RIGHT(-1, 1),
+    RIGHT(0, 1),
+    DOWN_RIGHT(1, 1),
+    DOWN(1, 0),
+    DOWN_LEFT(1, -1),
+    LEFT(0, -1);
+
+    private int rowChange;
+    private int colChange;
+
+    private RedDirection(int rowChange, int colChange) {
+        this.rowChange = rowChange;
+        this.colChange = colChange;
+    }
+
+    public int getRowChange() {
+        return rowChange;
+    }
+
+    public int getColChange() {
+        return colChange;
+    }
+
+    public static RedDirection of(int rowChange, int colChange) {
+        for (var direction : values()) {
+            if (direction.rowChange == rowChange && direction.colChange == colChange) {
+                return direction;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(of(1, -1));
+    }
+
+}
