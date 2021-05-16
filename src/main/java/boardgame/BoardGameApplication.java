578b573a872c0caff8a9f862680e26ab74259191
Szatmári Boldizsár <szatmariboldizsar@gmail.com>
2021-05-17 00:58:24 +0200

    BoardGameApplication.java updated

--- src/main/java/boardgame/BoardGameApplication.java
+++ src/main/java/boardgame/BoardGameApplication.java
@@ -1,12 +1,11 @@
 package boardgame;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class BoardGameApplication extends Application {
 
